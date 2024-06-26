9d4d05c33b18c064c067cbe5b1f1fed647bc543f
nishkakurupati <nishkakurupati@gmail.com>
2024-06-26 13:09:01 -0700

    Initial Commit

---
+++ 2024KWK_FinalProject_FutureFocus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2024KWK_FinalProject_FutureFocus
+//
+//  Created by Scholar on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2024KWK_FinalProject_FutureFocus/_024KWK_FinalProject_FutureFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  _024KWK_FinalProject_FutureFocusApp.swift
+//  2024KWK_FinalProject_FutureFocus
+//
+//  Created by Scholar on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct _024KWK_FinalProject_FutureFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
